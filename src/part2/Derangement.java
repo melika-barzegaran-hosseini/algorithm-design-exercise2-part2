78d295ed84414be71dcd45d726576a0b5c06fb88
melika barzegaran hosseini <melika.barzegaran.hosseini@gmail.com>
2016-02-12 13:06:26 +0330

    Add derangement recursive function
    it uses the equation D(n)=(n-1)(D(n-1)+D(n-2)) to calculate
    derangement of n recursively. the numerator of the fraction is computed.

---
+++ src/part2/Derangement.java
@@ -0,0 +1,57 @@
+package part2;
+
+public class Derangement
+{
+    private String input;
+
+    public Derangement(String input)
+    {
+        this.input = input;
+    }
+
+    private Long getDerangement(Long n) throws IllegalArgumentException
+    {
+        if(n < 0)
+        {
+            throw new IllegalArgumentException();
+        }
+        if(n == 0)
+        {
+            return 1L;
+        }
+        else if(n == 1)
+        {
+            return 0L;
+        }
+        else
+        {
+            return (n - 1) * (getDerangement(n - 1) + getDerangement(n - 2));
+        }
+    }
+
+    public void calculateProbability()
+    {
+        try
+        {
+            Long n = Long.parseLong(input);
+            Long numerator = getDerangement(n);
+            System.out.println("the numerator = " + numerator);
+        }
+        catch (NumberFormatException e)
+        {
+            System.err.println("error: the input is not a valid integer number.");
+            System.exit(1);
+        }
+        catch (IllegalArgumentException e)
+        {
+            System.err.println("error: the input is a not a positive integer number.");
+            System.exit(1);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Derangement derangement = new Derangement(args[0]);
+        derangement.calculateProbability();
+    }
+}
